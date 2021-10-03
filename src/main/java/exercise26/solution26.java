e4819116397e54ec2f44bc6faaedf9b8ee28eaca
JP Oliveira <joaopedro.costa.oliveira@gmail.com>
2021-10-03 18:37:57 -0400

    Pseudocode for 26

---
+++ src/main/java/exercise26/solution26.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 26 Solution
+ *  Copyright 2021 Joao Pedro Oliveira
+ */
+
+
+package exercise26;
+
+import java.util.*;
+import java.lang.*;
+
+public class solution26 {
+    /*
+    Print "What is your balance?"
+    balance = user input
+
+    Print "What is the apr on the card (as a percent)?"
+    APR = user input
+
+    Print "What is the monthly payment you can make?")
+    payment = user input
+
+    get daily value of APR
+    using the given formula, determine how many months it would take
+    round the months to the next whole month
+
+    Print ""It will take you " +months+ " months to pay off this card."
+
+     */
+
+
+
+    
+
+
+}
